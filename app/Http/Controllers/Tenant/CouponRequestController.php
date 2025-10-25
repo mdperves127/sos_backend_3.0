5ae41199c6bf2740b79e2c638c8dfd5073383149
abdur-shobur <abdur.shobur.me@gmail.com>
2025-10-25 15:38:02 +0600

    update coupon req  controller for previous logic

--- app/Http/Controllers/Tenant/CouponRequestController.php
+++ app/Http/Controllers/Tenant/CouponRequestController.php
@@ -3,29 +3,35 @@
 namespace App\Http\Controllers\Tenant;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Http\Requests\CouponSendRequest;
 use App\Models\CouponRequest;
+use Illuminate\Http\Request;
 
-class CouponRequestController extends Controller
-{
-    function store(CouponSendRequest $request)
-    {
-        CouponRequest::on('mysql')->create([
-            'comments' => request('comments'),
-            'tenant_id' => tenant()->id
-        ]);
+class CouponRequestController extends Controller {
+    function store( CouponSendRequest $request ) {
+        CouponRequest::on( 'mysql' )->create( [
+            'comments'  => request( 'comments' ),
+            'tenant_id' => tenant()->id,
+        ] );
 
-        return $this->response('Coupon request send successfully!');
+        return $this->response( 'Coupon request send successfully!' );
     }
 
-    function getcouponrequest()
-    {
-        $couponrequest = CouponRequest::on('mysql')
-            ->where('tenant_id', tenant()->id)
-            ->latest()
-            ->paginate(10);
+    function getcouponrequest() {
+        $couponrequest = CouponRequest::on( 'mysql' )
+            ->where( 'tenant_id', tenant()->id )
+            ->latest()->first();
 
-        return $this->response($couponrequest);
+        if ( $couponrequest ) {
+            return $this->response( $couponrequest );
+        }
+        return response()->json(
+            [
+                'status'  => 400,
+                'data'    => 'failed',
+                'message' => 'No coupon request found',
+            ]
+        );
+
     }
 }
