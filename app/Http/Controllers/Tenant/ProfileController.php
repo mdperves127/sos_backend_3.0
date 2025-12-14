945de4a991cc82b7b4c07ba8aceeb7ea61b066be
Md. Perves Hossen <105355533+mdperves127@users.noreply.github.com>
2025-12-14 20:28:56 +0600

    update

--- app/Http/Controllers/Tenant/ProfileController.php
+++ app/Http/Controllers/Tenant/ProfileController.php
@@ -18,58 +18,58 @@ class ProfileController extends Controller
         ]);
     }
 
-    // public function TenantUpdateProfile(Request $request)
-    // {
-    //     $validator =  Validator::make($request->all(), [
-    //         'name' => 'required',
-    //         'number' => 'required',
-    //         'number2' => 'nullable',
-    //         'old_password' => 'nullable',
-    //         'new_password' => 'nullable',
-    //     ]);
+    public function TenantUpdateProfile(Request $request)
+    {
+        $validator =  Validator::make($request->all(), [
+            'name' => 'required',
+            'number' => 'required',
+            'number2' => 'nullable',
+            'old_password' => 'nullable',
+            'new_password' => 'nullable',
+        ]);
 
-    //     if ($request->has('old_password') && $request->input('old_password') !== null) {
-    //         $validator->addRules([
-    //             'new_password' => 'required|min:8|max:32',
-    //         ]);
-    //     }
+        if ($request->has('old_password') && $request->input('old_password') !== null) {
+            $validator->addRules([
+                'new_password' => 'required|min:8|max:32',
+            ]);
+        }
 
-    //     if ($validator->fails()) {
-    //         return response()->json([
-    //             'status' => 400,
-    //             'message' => $validator->messages()
-    //         ]);
-    //     }
-
-
-    //     $data = User::find(Auth::user()->id);
-    //     $data->name = $request->name;
-    //     $data->number = $request->number;
-    //     $data->number2 = $request->number2;
-    //     if ($request->old_password) {
-    //         if (!Hash::check($request->old_password, auth()->user()->password)) {
-    //             return response()->json([
-    //                 'status' => 400,
-    //                 'message' => 'Old Password Not Match!'
-    //             ]);
-    //         } else {
-    //             $data->password = bcrypt($request->new_password);
-    //         }
-    //     }
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'message' => $validator->messages()
+            ]);
+        }
 
 
-    //     if ($request->hasFile('image')) {
-    //         if (File::exists($data->image)) {
-    //             File::delete($data->image);
-    //         }
-    //         $image =  fileUpload($request->image, 'uploads/vendor');
-    //         $data->image = $image;
-    //     }
+        $data = Tenant::find(tenant()->id);
+        $data->name = $request->name;
+        $data->number = $request->number;
+        $data->number2 = $request->number2;
+        if ($request->old_password) {
+            if (!Hash::check($request->old_password, auth()->user()->password)) {
+                return response()->json([
+                    'status' => 400,
+                    'message' => 'Old Password Not Match!'
+                ]);
+            } else {
+                $data->password = bcrypt($request->new_password);
+            }
+        }
 
-    //     $data->save();
-    //     return response()->json([
-    //         'status' => 200,
-    //         'message' => 'Profile updated Sucessfully!',
-    //     ]);
-    // }
+
+        if ($request->hasFile('image')) {
+            if (File::exists($data->image)) {
+                File::delete($data->image);
+            }
+            $image =  fileUpload($request->image, 'uploads/vendor');
+            $data->image = $image;
+        }
+
+        $data->save();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Profile updated Sucessfully!',
+        ]);
+    }
 }
