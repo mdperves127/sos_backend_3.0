4f16eb2cac0139242759b8c093d2369d74bf3a97
Md. Perves Hossen <105355533+mdperves127@users.noreply.github.com>
2026-01-17 17:02:37 +0600

    Update CouponController.php

--- app/Http/Controllers/API/Admin/CouponController.php
+++ app/Http/Controllers/API/Admin/CouponController.php
@@ -12,6 +12,7 @@ use App\Services\Admin\CouponService;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use App\Models\Tenant;
 
 class CouponController extends Controller {
     /**
