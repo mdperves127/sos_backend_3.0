4e8ed2c9fafa2f952361ad5deba6001852dfadec
Md. Perves Hossen <105355533+mdperves127@users.noreply.github.com>
2026-01-17 18:17:43 +0600

    Update ProfileController.php

--- app/Http/Controllers/Tenant/ProfileController.php
+++ app/Http/Controllers/Tenant/ProfileController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\UserSubscription;
 
 class ProfileController extends Controller
 {
