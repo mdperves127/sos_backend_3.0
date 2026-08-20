38f4477e35033b16c8c5eb83cd92519b970a6b0c
Md. Perves Hossen <105355533+mdperves127@users.noreply.github.com>
2026-08-20 23:31:31 +0600

    Update ProductService.php

--- app/Service/Vendor/ProductService.php
+++ app/Service/Vendor/ProductService.php
@@ -49,7 +49,7 @@ class ProductService {
                 $q->whereBetween( 'selling_price', [$min, $max] );
             } )
 
-            ->select( 'id', 'uniqid', 'image', 'name', 'selling_price', 'qty', 'status', 'created_at', 'discount_type', 'discount_rate', 'original_price', 'discount_price', 'is_affiliate', 'wc_product_id', 'product_type', 'is_stock_show', 'is_show_website' )
+            ->select( 'id', 'uniqid', 'image', 'name', 'selling_price', 'qty', 'status', 'created_at', 'discount_type', 'discount_rate', 'original_price', 'discount_price', 'is_affiliate', 'wc_product_id', 'product_type', 'is_stock_show', 'is_show_website', 'slug' )
             ->latest()
             ->paginate( 10 )
             ->withQueryString();
